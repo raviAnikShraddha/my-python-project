67cce0eda46dcefece391276c68fe2bfa510be37
RaviKashyap <ravipratapkashyap@gmail.com>
2025-05-04 12:27:33 +0530

    Initial commit!

---
+++ challenge.py
@@ -0,0 +1,13 @@
+'''
+# Celsius to Farenhiet
+c = input('Enter Degree celsius you want convert into farenhiet: ')
+c=float(c)
+f=(c*9/5)+32
+print('Farenhiet: ', f)
+'''
+
+# Farehiet to Celsius
+f = input('Enter Degree farenhiet: ')
+f = float(f)
+c = (f - 32) * 5/9
+print('Degree Celsius: ', c)
---
+++ simpleprogram.py
@@ -0,0 +1,5 @@
+#a = input('Please press any number: ')
+#print(f'Thanks for printing number: {a}')
+
+z = "I'm a polite person."
+print(z*3)
