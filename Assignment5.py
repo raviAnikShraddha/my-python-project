39dc5ff8cb40ed7b55744cec25b265b9afca0fa4
RaviKashyap <ravipratapkashyap@gmail.com>
2025-05-30 17:04:36 +0530

    Completing Assignment5.

---
+++ Assignment5.py
@@ -0,0 +1,25 @@
+# adding student name and marks as key value pairs in dictionary
+'''
+dict1 = {}
+key1 = str(input("Enter the name of Student : "))
+value1 = str(input("Enter the value of Student : "))
+dict1[key1] = value1
+key2 = str(input("Enter the name of Student : "))
+value2 = str(input("Enter the value of Student : "))
+dict1[key2] = value2
+
+def function(key):
+    global dict1
+    if key not in dict1:
+        print("Student not found.")
+
+function(input("Enter the name of Student : "))
+
+
+# creating list
+list1 = [1,2,3,4,5,6,7,8,9,10]
+list2 = list1[0:5]
+print("Extracted first five elements : ", list2)
+print("Reversed extracted elements : ", list2[::-1])
+
+'''
